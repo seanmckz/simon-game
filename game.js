0ced59cccb1fd9f0d65c393d041cd81aff152a6e
sean w mckenzie <seanwyattmckenzie@gmail.com>
2022-10-03 00:14:42 -0800

    Added randomisation components to the game logic

---
+++ game.js
@@ -0,0 +1,10 @@
+var gamePattern = []
+gamePattern.push(randomChosenColor)
+
+function nextSequence(){
+   return randomNumber = Math.floor(Math.random()*4)
+
+}
+
+var buttonsColors = ["red", "green", "blue", "yellow"]
+var randomChosenColor = buttonsColors[nextSequence()]
