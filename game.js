9073aab9079100bcfc0448cd718fb97f0c4d5a95
sean w mckenzie <seanwyattmckenzie@gmail.com>
2022-10-09 12:42:30 -0800

    completed vsersion

--- game.js
+++ game.js
@@ -1,54 +1,79 @@
-var userClickedPattern = []
+var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+var userClickedPattern = [];
 
-var gamePattern = []
-gamePattern.push(randomChosenColor)
+var started = false;
+var level = 0;
 
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
-function nextSequence(){
-   return randomNumber = Math.floor(Math.random()*4)
+$(".btn").click(function() {
 
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
 }
 
-var buttonsColors = ["red", "green", "blue", "yellow"]
-var randomChosenColor = buttonsColors[nextSequence()]
 
-// Concatenating string for button Key Selection
-var key = "#" + randomChosenColor
-$(key).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200)
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-$(".btn").on("click", function(){
-   var userChosenColor = this.id
-   userClickedPattern.push(userChosenColor)
-   console.log(userClickedPattern)
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
 
-   switch (this.id) {
-      case "red":
-         var red = new Audio("sounds/red.mp3")
-         red.play()
-         break;
-   
-      case "green":
-         var green = new Audio("sounds/green.mp3")
-         green.play()
-         break;
-   
-      case "blue":
-         var blue = new Audio("sounds/blue.mp3")
-         blue.play()
-         break;
-   
-      case "yellow":
-         var yellow = new Audio("sounds/yellow.mp3")
-         yellow.play()
-         break;
-   
-      default:
-         break;
-   }
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
 
-})
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
 
-
-// $("#green").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200)
